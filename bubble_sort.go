3b1f2c5025242af5bf7d9060bc54a0e31a360d2d
AlexandruMitcul <alexandru.mitcul@outlook.com>
2024-08-13 18:57:59 +0100

    Refactored the go files

--- bubble_sort.go
+++ bubble_sort.go
@@ -19,9 +19,3 @@ func bubbleSort(arr []int) []int {
 	return arr
 }
 
-func main() {
-	arr := []int{64, 34, 25, 12, 22, 11, 90}
-	sortedArr := bubbleSort(arr)
-	fmt.Println("Sorted array is:", sortedArr)
-}
-
---
+++ main.go
@@ -0,0 +1,6 @@
+func main() {
+	arr := []int{64, 34, 25, 12, 22, 11, 90}
+	sortedArr := bubbleSort(arr)
+	fmt.Println("Sorted array is:", sortedArr)
+}
+
