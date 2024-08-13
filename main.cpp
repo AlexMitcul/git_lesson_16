c6acbe0b173400ebb19bcf410c8023b072081449
AlexandruMitcul <alexandru.mitcul@outlook.com>
2024-08-13 19:00:37 +0100

    data was modified

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 int main() {
-    std::vector<int> arr2 = {1,2,3,4,5};
+    std::vector<int> arr2 = {1,1,1,1,1,1};
     bubbleSort(arr2);
     std::cout << "Sorted array is: ";
     for (int i = 0; i < arr2.size(); ++i)
